1e28b806e0a9443c83db48cfbea6e1812c900f7b
Agniva Mukherjee <agnivamukherjee977@gmail.com>
2026-01-21 13:32:01 +0530

    Update client API endpoint URL

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('https://cipher-chat-pi.vercel.app').api
+export const client = treaty<App>('https://chatapp.myportfolioagnivaapp.tech').api
